dc14d1d5f6a048db5fae6b1a060fd6968ff4ecce
lekdup <lekdup@live.fr>
2023-10-15 22:52:10 +0200

    deck of cards created with the loop paquet[i] = valeur[i%13] + couleur[i/13]

--- JAVA/Bataille de cartes/src/Entities/Carte.java
+++ JAVA/Bataille de cartes/src/Entities/Carte.java
@@ -1,32 +1,41 @@
 package Entities;
 
 public class Carte {
-    private int valeur;
-    private String couleur;
+    String[] couleur = {"COEUR", "CARREAUX", "TRÈFLE", "PIQUE"};
+    String[] valeur = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R", "A"};
+    String[] paquet = new String[52];
+
+    // create the deck of cards
+
 
     //int nombreCarte = valeur.length * couleur.length;
     // constructor that will buil a full deck of cards
-    public Carte(String couleur, int valeur) {
-        this.couleur = couleur;
-        this.valeur = valeur;
+
+    public Carte() {
+        for (int i = 0; i < paquet.length; i++) {
+            paquet[i] = valeur[i%13] + " " + couleur[i/13];
+            System.out.println(paquet[i]);
+        }
     }
     //create getters for suits and ranks
+    /*
     public String getCouleur() {
         return couleur;
     }
 
-    public int getValeur() {
+    //public int getValeur() {
         return valeur;
     }
 
     //create setters for suits and ranks
-    public void setCouleur(String couleur) {
+    //public void setCouleur(String couleur) {
         this.couleur = couleur;
     }
 
-    public void setValeur(int valeur) {
+    //public void setValeur(int valeur) {
         this.valeur = valeur;
     }
 
     //create comparator
+                */
 }
--- JAVA/Bataille de cartes/src/Run/Bataille.java
+++ JAVA/Bataille de cartes/src/Run/Bataille.java
@@ -4,7 +4,8 @@ import Entities.Carte;
 
 public class Bataille {
     public static void main(String[] args) {
-        Carte carte = new Carte("Pique", 10);
-        System.out.println(carte);
+        //Carte carte = new Carte("Pique", 10);
+        Carte cards = new Carte();
+        System.out.println(cards);
     }
 }
